96498f06d8a928495a99a5c8884df2b0dde18ca3
KrishnaB22 <krishnabhandarge@gmail.com>
2022-10-13 12:52:26 +0530

    update

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ myfiles = os.listdir('inputs/')
 ops = ['add','read','delete']
 
 f = open("commands.txt",'w')
-
+addedlist = []
 
 for i in range(100):
 
@@ -18,8 +18,12 @@ for i in range(100):
 
     if action == 'read':
         outname = 'outputs/' + outname
+        fname = random.choice(addedlist)
     else:
         outname = 'inputs/' + outname
+
+    if action == 'add':
+        addedlist.append(fname)
 
 
     f.write("{}\n{}\n{}\n".format(action,fname,outname))
